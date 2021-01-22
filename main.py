96b6885f7568ef96456cc7559e1c54d1e3eda3ab
Dmitry <ludaholodec@gmail.com>
2021-01-22 17:40:19 +0400

    finally2

--- main.py
+++ main.py
@@ -80,8 +80,6 @@ async def help(message: types.Message):
 
 @dp.message_handler()
 async def wrong(message: types.Message, state: FSMContext):
-    st = await state.get_state()
-    await bot.send_message(message.chat.id, st)
     await bot.send_message(message.chat.id, WRONG_COMMAND, reply_markup=kb)
 
 
