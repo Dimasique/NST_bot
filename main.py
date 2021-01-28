6d20d86e6b2f2f122f10aec17b69d32e8d64e418
Dmitry <ludaholodec@gmail.com>
2021-01-28 20:55:07 +0300

    aaaa

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ async def incoming_conten_gan(message: types.message, state: FSMContext):
         await bot.send_message(message.chat.id, WORKING, reply_markup=kb)
 
         with open('file.txt') as f:
-            path = f.read()
+            path = f.read()[:-1]
         answer = InputFile(path_or_bytesio=path)
 
         nst.run_gan(content.file_id)
