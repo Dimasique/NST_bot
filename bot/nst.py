1fe5e57907816b629022cd3cf6c75e907afdbc7e
Dmitry <ludaholodec@gmail.com>
2021-01-29 17:21:01 +0300

    fix path5

--- bot/nst.py
+++ bot/nst.py
@@ -53,5 +53,5 @@ def save(img):
 
 
 def run_gan(img, model):
-    os.system(f'python ./GAN/test.py --dataroot ./images/ --name {model}_pretrained --model test --no_dropout')
+    os.system(f'python ./GAN/test.py --dataroot ./bot/images/ --name {model}_pretrained --model test --no_dropout')
     os.remove(f'bot/images/{img}.jpg')
