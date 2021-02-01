9ed84d0f7219a9745b1c9b24b3849ef0070d7ec1
Dmitry <ludaholodec@gmail.com>
2021-02-01 15:26:12 +0300

    refactoring1

--- bot/main.py
+++ bot/main.py
@@ -40,30 +40,33 @@ dp.middleware.setup(LoggingMiddleware())
 
 @dp.message_handler(commands=['start'], state="*")
 async def start(message: types.Message):
-    await message.answer(HELLO, reply_markup=kb)
-    # await bot.send_message(message.chat.id, HELLO, reply_markup=kb)
+    await message.answer(START, reply_markup=kb)
 
 
 @dp.message_handler(commands=['help'], state="*")
 async def help(message: types.Message):
     await message.answer(HELP)
-    # await bot.send_message(message.chat.id, HELP, reply_markup=kb)
+
+
+@dp.message_handler(commands=['nst'], state="*")
+async def choose_nst(message: types.Message):
+    await NST_States.waiting_for_content.set()
+    await message.answer(CHOOSE_NST)
+
+
+@dp.message_handler(commands=['gan'], state="*")
+async def choose_gan(message: types.message):
+    await GAN_States.waiting_for_painter.set()
+    await message.answer(CHOOSE_GAN, reply_markup=gan_kb)
 
 
 @dp.message_handler(commands=['cancel'], state="*")
 async def cancel(message: types.Message, state: FSMContext):
     await state.finish()
     await message.answer(CANCEL, reply_markup=kb)
-    # await bot.send_message(message.chat.id, CANCEL, reply_markup=kb)
 
 
 # __________________________NST_________________________________________#
-
-@dp.message_handler(commands=['nst'], state="*")
-async def choose_nst(message: types.Message):
-    await NST_States.waiting_for_content.set()
-    await message.answer(NST_CHOOSE)
-    # await bot.send_message(message.chat.id, NST_CHOOSE, reply_markup=kb)
 
 
 @dp.message_handler(state=NST_States.waiting_for_content, content_types=ContentType.ANY)
@@ -71,11 +74,9 @@ async def incoming_content_nst(message: types.message, state: FSMContext):
     if len(message.photo) > 0:
         await state.update_data(content=message.photo[-1])
         await NST_States.waiting_for_style.set()
-        await message.answer(WAIT_FOR_STYLE)
-        # await bot.send_message(message.chat.id, WAIT_FOR_STYLE, reply_markup=kb)
+        await message.answer(WAITING_FOR_IMAGE_STYLE)
     else:
-        await message.answer(GETTING_IMAGE_ERROR)
-        # await bot.send_message(message.chat.id, GETTING_IMAGE_ERROR, reply_markup=kb)
+        await message.answer(GETTING_IMAGE_CONTENT_ERROR)
 
 
 @dp.message_handler(state=NST_States.waiting_for_style, content_types=ContentType.ANY)
@@ -92,8 +93,6 @@ async def incoming_style_nst(message: types.message, state: FSMContext):
         await style.download(style_name)
         await content.download(content_name)
 
-
-        # await bot.send_message(message.chat.id, WORKING, reply_markup=kb)
         style_transfer.run_nst(style_name, content_name)
         answer = InputFile(path_or_bytesio='bot/result/res.jpg')
         await bot.send_photo(message.chat.id, answer, DONE)
@@ -101,8 +100,7 @@ async def incoming_style_nst(message: types.message, state: FSMContext):
         await state.finish()
 
     else:
-        await message.answer(GETTING_IMAGE_ERROR)
-        # await bot.send_message(message.chat.id, GETTING_IMAGE_ERROR, reply_markup=kb)
+        await message.answer(GETTING_IMAGE_STYLE_ERROR)
 
 
 # _______________________________________________________________________#
@@ -110,17 +108,12 @@ async def incoming_style_nst(message: types.message, state: FSMContext):
 
 # __________________________GAN_________________________________________#
 
-@dp.message_handler(commands=['gan'], state="*")
-async def choose_gan(message: types.message):
-    await GAN_States.waiting_for_painter.set()
-    await message.answer(GAN_CHOOSE, reply_markup=gan_kb)
-    # await bot.send_message(message.chat.id, GAN_CHOOSE, reply_markup=gan_kb)
 
 @dp.callback_query_handler(lambda c: c.data == 'vangogh', state=GAN_States.waiting_for_painter)
 async def process_callback_vangogh(callback_query: types.CallbackQuery, state: FSMContext):
     await bot.answer_callback_query(callback_query.id)
     await GAN_States.waiting_for_content.set()
-    await bot.send_message(callback_query.from_user.id, WAITING_FOR_IMAGE)
+    await bot.send_message(callback_query.from_user.id, WAITING_FOR_IMAGE_GAN)
     await state.update_data(model='style_vangogh')
 
 
@@ -128,7 +121,7 @@ async def process_callback_vangogh(callback_query: types.CallbackQuery, state: F
 async def process_callback_monet(callback_query: types.CallbackQuery, state: FSMContext):
     await bot.answer_callback_query(callback_query.id)
     await GAN_States.waiting_for_content.set()
-    await bot.send_message(callback_query.from_user.id, WAITING_FOR_IMAGE)
+    await bot.send_message(callback_query.from_user.id, WAITING_FOR_IMAGE_GAN)
     await state.update_data(model='style_monet')
 
 
@@ -140,9 +133,8 @@ async def incoming_content_gan(message: types.message, state: FSMContext):
         content_name = f'bot/images/{content.file_id}.jpg'
 
         await content.download(content_name)
-
         await message.answer(WORKING)
-        # await bot.send_message(message.chat.id, WORKING, reply_markup=kb)
+
         data = await state.get_data()
 
         style_transfer.run_gan(content.file_id, data['model'])
@@ -153,7 +145,7 @@ async def incoming_content_gan(message: types.message, state: FSMContext):
         await state.finish()
 
     else:
-        await bot.send_message(message.chat.id, GETTING_IMAGE_ERROR, reply_markup=kb)
+        await bot.send_message(message.chat.id, GETTING_IMAGE_CONTENT_ERROR, reply_markup=kb)
 
 
 # _______________________________________________________________________#
@@ -162,7 +154,6 @@ async def incoming_content_gan(message: types.message, state: FSMContext):
 @dp.message_handler(state="*", content_types=ContentType.ANY)
 async def wrong_message(message: types.message):
     await message.answer(WRONG_COMMAND, reply_markup=kb)
-    # await bot.send_message(message.chat.id, WRONG_COMMAND, reply_markup=kb)
 
 
 async def on_startup(dp):
@@ -172,8 +163,6 @@ async def on_startup(dp):
 
 async def on_shutdown(dp):
     pass
-
-
 
 
 if __name__ == '__main__':
