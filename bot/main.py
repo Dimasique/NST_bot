f16acbd595ca054accc56729e8cd6a32c86fe5ff
Dmitry <ludaholodec@gmail.com>
2021-02-06 14:00:48 +0300

    try thread4

--- bot/main.py
+++ bot/main.py
@@ -201,7 +201,7 @@ def process_queue(task_queue):
 
 
 if __name__ == '__main__':
-    worker = Thread(target=process_queue, args=(task_queue, result_queue,))
+    worker = Thread(target=process_queue, args=(task_queue,))
     worker.start()
 
     start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH, on_startup=on_startup, on_shutdown=on_shutdown,
